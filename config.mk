9ed7da55ab05f314067807a6b5f96a54cd6b4091
em <em>
2018-02-11 20:24:59 +0800

    update brpc

--- config.mk
+++ config.mk
@@ -2,3 +2,12 @@ CC= gcc
 CXX= g++
 PWD= $(shell pwd)
 SRC= ..
+BRPC_SRC_DIR= $(SRC)/brpc
+
+brpc-conf:$(BRPC_SRC)/config_brpc.sh
+	@echo "Configuring brpc"
+	@cd $(BRPC_SRC_DIR) && sh ./config_brpc.sh --headers=/usr/include --libs=/usr/lib
+
+brpc: $(BRPC_SRC_DIR)/Makefile brpc-conf
+	@echo "Building brpc"
+	@make -C $(BRPC_SRC_DIR)
